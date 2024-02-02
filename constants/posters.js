228c5b6396048ecb638729d192f112e9a68cf9a4
vivinkv6 <vivinkv0@gmail.com>
2024-02-02 07:53:12 +0530

    update

--- constants/posters.js
+++ constants/posters.js
@@ -1,56 +1,57 @@
 const posters = [
   {
     id: 1,
-    name: "choreography",
-    image: "/images/poster1.jpg",
-  },
-  {
-    id: 2,
-    name: "cipher",
-    image: "/images/poster2.jpg",
-  },
-  {
-    id: 3,
-    name: "codex",
-    image: "/images/poster3.jpg",
-  },
-  {
-    id: 6,
-    name: "crimeinvestigation",
-    image: "/images/poster3.jpg",
-  },
-  {
-    id: 5,
-    name: "itquiz",
-    image: "/images/poster2.jpg",
-  },
-  {
-    id: 4,
-    name: "gaming",
-    image: "/images/poster1.jpg",
-  },
-  {
-    id: 1,
     name: "band",
     image: "/images/poster1.jpg",
   },
   {
     id: 2,
-    name: "photography",
-    image: "/images/poster2.jpg",
+    name: "choreography",
+    image: "/images/poster3.jpg",
   },
   {
     id: 3,
-    name: "reconcile",
+    name: "cipher",
+    image: "/images/poster2.jpg",
+  },
+  {
+    id: 4,
+    name: "codex",
+    image: "/images/poster2.jpg",
+  },
+  {
+    id: 5,
+    name: "crimeinvestigation",
     image: "/images/poster3.jpg",
   },
   {
     id: 6,
+    name: "itquiz",
+    image: "/images/poster3.jpg",
+  },
+  {
+    id: 7,
+    name: "gaming",
+    image: "/images/poster3.jpg",
+  },
+ 
+  {
+    id: 8,
+    name: "photography",
+    image: "/images/poster2.jpg",
+  },
+  {
+    id: 9,
+    name: "reconcile",
+    image: "/images/poster3.jpg",
+  },
+  {
+    id: 10,
     name: "football",
     image: "/images/poster3.jpg",
   },
   {
-    id: 5,
+    id: 11,
     name: "webcast",
     image: "/images/poster2.jpg",
   },
