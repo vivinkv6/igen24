2ac59f5c67c7691345440798e8b69a8fbbb31e31
vivinkv6 <vivinkv0@gmail.com>
2024-02-03 21:24:18 +0530

    update

--- constants/posters.js
+++ constants/posters.js
@@ -3,62 +3,74 @@ const posters = [
     id: 1,
     name: "band",
     image: "/images/poster1.jpg",   
+    pdf:""
   },
   {
     id: 2,
     name: "choreography",
     image: "/images/poster5.jpg",
+    pdf:""
   },
   {
     id: 3,
     name: "cipher",
     image: "/images/poster8.jpg",
+    pdf:""
   },
   {
     id: 4,
     name: "codex",
     image: "/images/poster2.jpg",
+    pdf:""
   },
   {
     id: 5,
     name: "crimeinvestigation",
     image: "/images/poster3.jpg",
+    pdf:""
   },
   {
     id: 6,
     name: "itquiz",
     image: "/images/poster6.jpg",
+    pdf:""
   },
   {
     id: 7,
     name: "gaming",
     image: "/images/poster7.jpg",
+    pdf:""
   },
  
   {
     id: 8,
     name: "photography",
     image: "/images/poster9.jpg",
+    pdf:""
   },
   {
     id: 9,
     name: "reconcile",
     image: "/images/poster4.jpg",
+    pdf:""
   },
   {
     id: 10,
     name: "football",
     image: "/images/poster10.jpg",
+    pdf:""
   },
   {
     id: 11,
     name: "webcast",
     image: "/images/poster11.jpg",
+    pdf:""
   },
   {
     id: 12,
     name: "trivia",
     image: "/images/poster12.jpg",
+    pdf:""
   },
 ];
 
