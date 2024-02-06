163320890a84da20865e9554bb146db9a60d9ca7
vivinkv6 <vivinkv0@gmail.com>
2024-02-06 06:28:51 +0530

    add new banners

--- constants/posters.js
+++ constants/posters.js
@@ -3,84 +3,80 @@ const day1 = [
     id: 2,
     name: "choreography",
     image: "/images/poster5.jpg",
-    pdf:"/images/guideline2.jpg"
+    pdf: "/images/guideline2.jpg",
   },
   {
     id: 3,
     name: "cipher",
     image: "/images/poster8.jpg",
-    pdf:"/images/guideline6.jpg"
+    pdf: "/images/guideline6.jpg",
   },
-
 
   {
     id: 6,
     name: "itquiz",
     image: "/images/poster6.jpg",
-    pdf:"/images/guideline5.jpg"
+    pdf: "/images/guideline5.jpg",
   },
 
- 
   {
     id: 8,
     name: "photography",
     image: "/images/poster9.jpg",
-    pdf:""
+    pdf: "/images/guideline9.jpg",
   },
- 
+
   {
     id: 10,
     name: "football",
     image: "/images/poster10.jpg",
-    pdf:"/images/guideline3.jpg"
+    pdf: "/images/guideline3.jpg",
   },
   {
     id: 11,
     name: "webcast",
     image: "/images/poster11.jpg",
-    pdf:""
+    pdf: "/images/guideline10.jpg",
   },
-
 ];
 
 const day2 = [
   {
     id: 1,
     name: "band",
-    image: "/images/poster1.jpg",   
-    pdf:"/images/guideline1.jpg"
+    image: "/images/poster1.jpg",
+    pdf: "/images/guideline1.jpg",
   },
   {
     id: 4,
     name: "codex",
     image: "/images/poster2.jpg",
-    pdf:""
+    pdf: "/images/guideline11.jpg",
   },
   {
     id: 5,
     name: "crimeinvestigation",
     image: "/images/poster3.jpg",
-    pdf:"/images/guideline4.jpg"
+    pdf: "/images/guideline4.jpg",
   },
   {
     id: 7,
     name: "gaming",
     image: "/images/poster7.jpg",
-    pdf:"/images/guideline8.jpg"
+    pdf: "/images/guideline8.jpg",
   },
   {
     id: 9,
     name: "reconcile",
     image: "/images/poster4.jpg",
-    pdf:"/images/guideline7.jpg"
+    pdf: "/images/guideline7.jpg",
   },
   {
     id: 12,
     name: "trivia",
     image: "/images/poster12.jpg",
-    pdf:""
+    pdf: "/images/guideline12.jpg",
   },
 ];
 
-
-module.exports = {day1,day2};
+module.exports = { day1, day2 };
